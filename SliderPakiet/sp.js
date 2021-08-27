ce5aaa843312ef59a8afdfdd8f5c5f11eebd4bbc
bibliotekazsoms <89656930+bibliotekazsoms@users.noreply.github.com>
2021-08-27 21:54:36 +0200

    Delete SliderPakiet directory

--- SliderPakiet/sp.js
+++
@@ -1,6 +0,0 @@
-jQuery(document).ready(function(){
-	jQuery(".likebox_1").hover(function(){ jQuery(this).stop(true,false).animate({right:  0}, 700); },function(){ jQuery(".likebox_1").stop(true,false).animate({right: -344}, 700); });
-	jQuery(".tw_1").hover(function(){ jQuery(this).stop(true,false).animate({right:  0}, 700); },function(){ jQuery(".tw_1").stop(true,false).animate({right: -295}, 700); });
-	jQuery(".polecam_1").hover(function(){ jQuery(this).stop(true,false).animate({right:  0}, 700); },function(){ jQuery(".polecam_1").stop(true,false).animate({right: -200}, 700); });
-	jQuery(".gplus").hover(function(){ jQuery(this).stop(true,false).animate({right:  0}, 700); },function(){ jQuery(".gplus").stop(true,false).animate({right: -200}, 700); });
-});
